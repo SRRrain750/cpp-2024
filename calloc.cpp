7f0f1b8a1f24ddde5ad5dd0c4bd834c101a12023
Aabhishek23 <95581248+Aabhishek23@users.noreply.github.com>
2024-03-21 12:40:36 +0530

    Update calloc.cpp

--- calloc.cpp
+++ calloc.cpp
@@ -17,11 +17,10 @@ for (int i=0;i<5; i++){
 }
 
 //set intial position of pointer
-for(int k=5;k>0;k--){
-    cout<<*p<<" ";
+for(int k=4;k>0;k--){
     p--;
-
 }
+
 
 //print value of dynamic array
 for (int j=0;j<5; j++){
