8b5c3701a9e753ded37813fd92e2acf7cff82466
SRRrain750 <160513264+SRRrain750@users.noreply.github.com>
2024-03-21 12:06:53 +0530

    Add files via upload

---
+++ calloc.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int  *p=(int*)calloc(5,sizeof(int));
+for (int i=0;i<5; i++){
+    cout<<"enter the value:";
+    cin>>*p;
+    if(i!=4)
+    {
+
+
+    p++;
+    }
+}
+
+for(int i=4;i<=0;i--){
+    cout<<*p<<" ";
+    p--;
+
+}
+return 0;
+}
